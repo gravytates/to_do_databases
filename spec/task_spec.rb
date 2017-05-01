e3850e4a48509000b28adc6c489f87b21d53bd3a
Grady L Shelton <gradyish@gmail.com>
2017-04-30 22:54:58 -0700

    update database methods to nest tasks within lists and refactor tests

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,26 +1,9 @@
-require("rspec")
-require("pg")
-require("task")
-
-DB = PG.connect({:dbname => 'to_do_test'})
-
-RSpec.configure do |config|
-  config.after(:each) do
-    DB.exec("DELETE FROM tasks *;")
-  end
-end
+require('spec_helper')
 
 describe(Task) do
-  before() do
-    Task.clear()
-  end
-
-  describe("#description") do
-    it("lets you read the description out") do
-      test_task = Task.new("scrub the zebra")
-      expect(test_task.description()).to(eq("scrub the zebra"))
-    end
-  end
+  # before() do
+  #   Task.clear()
+  # end
 
   describe('.all') do
     it("is empty at first") do
@@ -30,24 +13,38 @@ describe(Task) do
 
   describe("#save") do
     it("adds a task to the array of saved tasks") do
-      test_task = Task.new("wash the lion")
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
   end
 
-  describe(".clear") do
-    it("empties out all of the saved tasks") do
-      Task.new("wash the lion").save()
-      Task.clear()
-      expect(Task.all()).to(eq([]))
+  describe("#description") do
+    it("lets you read the description out") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.description()).to(eq("learn SQL"))
     end
   end
 
+  describe("#list_id") do
+    it('lets you read the list ID out') do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      expect(test_task.list_id()).to(eq(1))
+    end
+  end
+
+  # describe(".clear") do
+  #   it("empties out all of the saved tasks") do
+  #     Task.new("wash the lion").save()
+  #     Task.clear()
+  #     expect(Task.all()).to(eq([]))
+  #   end
+  # end
+
   describe("#==") do
     it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL"})
-      task2 = Task.new({:description => "learn SQL"})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1})
       expect(task1).to(eq(task2))
     end
   end
